852daf6f529f6e7a4c277dfa9295c09c416009fb
Vadim Chistiakov <v.chistyakov@prequelapp.com>
2023-02-01 20:05:08 +0100

    initial commit

--- PinterestCompositionalLayout/ViewController.swift
+++ PinterestCompositionalLayout/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Pinterest Compositional Layout"
+        view.backgroundColor = .lightGray
     }
-
 
 }
 
