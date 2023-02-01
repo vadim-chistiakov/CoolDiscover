800cd009ceb3cc79fa46b775b1c95c4c516529ac
Vadim Chistiakov <v.chistyakov@prequelapp.com>
2023-02-01 19:46:24 +0100

    Initial Commit

---
+++ PinterestCompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinterestCompositionalLayout
+//
+//  Created by Vadim Chistiakov on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
