7d8c1ac0eee41ba7aa8ecf5e39b5586fb2d15758
Vadim Chistiakov <v.chistyakov@prequelapp.com>
2023-02-06 21:03:17 +0100

    added others layout

--- PinterestCompositionalLayout/Pinterest/PinterestViewController.swift
+++ PinterestCompositionalLayout/Pinterest/PinterestViewController.swift
@@ -12,8 +12,10 @@ import Combine
 typealias DataSource = UICollectionViewDiffableDataSource<Section, PictureModel>
 typealias DataSourceSnapshot = NSDiffableDataSourceSnapshot<Section, PictureModel>
 
-enum Section {
-    case main
+enum Section: Int, CaseIterable {
+    case carousel
+    case widget
+    case pinterest
 }
 
 final class PinterestViewController: UIViewController, UICollectionViewDelegate {
@@ -35,9 +37,13 @@ final class PinterestViewController: UIViewController, UICollectionViewDelegate 
     }
     
     private lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: UICollectionViewLayout()
+        )
         collectionView.delegate = self
-        collectionView.backgroundColor = .systemBackground
+        collectionView.backgroundColor = .systemGray
+        collectionView.showsHorizontalScrollIndicator = false
         collectionView.register(PictureCell.self, forCellWithReuseIdentifier: Const.cellId)
         return collectionView
     }()
@@ -83,61 +89,101 @@ final class PinterestViewController: UIViewController, UICollectionViewDelegate 
             .store(in: &cancellables)
     }
     
-    private func configureLayout(ratios: [CGFloat]) {
-        collectionView.setCollectionViewLayout(
-            createLayout(ratios: ratios),
-            animated: false
-        )
+    private func configureLayout(ratios: [Ratioable]) {
+        let layout = UICollectionViewCompositionalLayout { [weak self] sectionIndex, enviroment in
+            guard let self,
+                    let section = Section(rawValue: sectionIndex)
+            else { return nil }
+            switch section {
+            case .carousel :
+                return self.carouselBannerSection()
+            case .widget :
+                return self.widgetBannerSection()
+            case .pinterest:
+                return self.pinterestSection(ratios: ratios)
+            }
+        }
+        collectionView.setCollectionViewLayout(layout, animated: true)
     }
 
 }
 
 extension PinterestViewController {
-
-    private func createUsialLayout() -> UICollectionViewLayout  {
+    
+    private func carouselBannerSection() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(
-            widthDimension:  .fractionalWidth(0.5),
-            heightDimension: .fractionalWidth(0.5)
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
         )
-
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
 
         let groupSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1),
-            heightDimension: .fractionalWidth(0.5)
+            heightDimension: .fractionalWidth(1)
         )
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize:  groupSize, subitems: [item])
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
-        
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
+        section.orthogonalScrollingBehavior = .continuous
+        section.visibleItemsInvalidationHandler = { (items, offset, environment) in
+            items.forEach { item in
+                let distanceFromCenter = abs((item.frame.midX - offset.x) - environment.container.contentSize.width / 2.0)
+                let minScale: CGFloat = 0.8
+                let maxScale: CGFloat = 1.0
+                let scale = max(maxScale - (distanceFromCenter / environment.container.contentSize.width), minScale)
+                item.transform = CGAffineTransform(scaleX: scale, y: scale)
+            }
+        }
+        return section
+    }
+    
+    private func widgetBannerSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.2),
+            heightDimension: .fractionalWidth(0.3)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 10, leading: 5, bottom: 10, trailing: 5)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
     }
                             
-    private func createLayout(ratios: [CGFloat]) -> UICollectionViewLayout {
-        let pinterestLayout = PinterestLayout()
-        let section = pinterestLayout.makeVariousAspectRatioLayoutSection(
+    private func pinterestSection(ratios: [Ratioable]) -> NSCollectionLayoutSection {
+        PinterestLayout().makeVariousAspectRatioLayoutSection(
             columnsCount: 2,
             itemRatios: ratios,
             spacing: 10,
             contentWidth: view.bounds.width
         )
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
     }
     
     private func configureDataSource() {
         viewModel.dataSource = DataSource(
             collectionView: collectionView,
             cellProvider: { [weak self] (collectionView, indexPath, model) -> PictureCell? in
-                guard let self else { return .init() }
+                guard let self,
+                      let section = Section(rawValue: indexPath.section)
+                else { return .init() }
                 let cell = collectionView.dequeueReusableCell(
                     withReuseIdentifier: Const.cellId, for: indexPath) as! PictureCell
                 cell.imageView.image = UIImage(blurHash: model.blurHash, size: model.blurHashSize)
                 
                 //TODO: - Как лучше организовать запрос картинок?
-                self.viewModel.loadImage(for: indexPath.item)
+                
+                self.viewModel.loadImage(for: indexPath.item, inSection: section)
                     .sink { _ in }
                     receiveValue: { data in
                         cell.imageView.image = UIImage(data: data)
@@ -145,6 +191,7 @@ extension PinterestViewController {
                     .store(in: &self.cancellables)
 
                 return cell
-            })
+            }
+        )
     }
 }
--- PinterestCompositionalLayout/Pinterest/PinterestViewModel.swift
+++ PinterestCompositionalLayout/Pinterest/PinterestViewModel.swift
@@ -13,9 +13,9 @@ protocol PinterestViewModel: AnyObject {
     var dataSource: DataSource! { get set }
     var isRefreshing: Bool { get }
     
-    func refresh() -> AnyPublisher<[CGFloat], Never>
-    func loadImages(animatingDifferences: Bool) -> AnyPublisher<[CGFloat], Never>
-    func loadImage(for index: Int) -> AnyPublisher<Data, RequestError>
+    func refresh() -> AnyPublisher<[Ratioable], Never>
+    func loadImages(animatingDifferences: Bool) -> AnyPublisher<[Ratioable], Never>
+    func loadImage(for index: Int, inSection section: Section) -> AnyPublisher<Data, RequestError>
 }
 
 final class PinterestViewModelImpl: PinterestViewModel {
@@ -32,7 +32,7 @@ final class PinterestViewModelImpl: PinterestViewModel {
         self.imagesNetworkService = imagesNetworkService
     }
     
-    func loadImages(animatingDifferences: Bool = false) -> AnyPublisher<[CGFloat], Never> {
+    func loadImages(animatingDifferences: Bool = false) -> AnyPublisher<[Ratioable], Never> {
         imagesNetworkService.getImages(page: (1...10).randomElement() ?? 1)
             .receive(on: DispatchQueue.main)
             .handleEvents(receiveOutput: { [weak self] pictures in
@@ -45,18 +45,19 @@ final class PinterestViewModelImpl: PinterestViewModel {
                     print("error \(error.debugDescription)")
                 }
             })
-            .map { $0.map { $0.ratio } }
+//            .map { $0[($0.count/2)+1...$0.count-1] }
+            .map { $0.map { $0 as Ratioable }}
             .replaceError(with: [])
             .eraseToAnyPublisher()
     }
     
-    func loadImage(for index: Int) -> AnyPublisher<Data, RequestError> {
-        imagesNetworkService.getImage(urlString: snapshot.itemIdentifiers[index].urls.small)
+    func loadImage(for index: Int, inSection section: Section) -> AnyPublisher<Data, RequestError> {
+        imagesNetworkService.getImage(urlString: snapshot.itemIdentifiers(inSection: section)[index].urls.small)
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
-    func refresh() -> AnyPublisher<[CGFloat], Never> {
+    func refresh() -> AnyPublisher<[Ratioable], Never> {
         isRefreshing = true
         return loadImages(animatingDifferences: true)
     }
@@ -65,8 +66,12 @@ final class PinterestViewModelImpl: PinterestViewModel {
     
     private func configureDataSource(pictures: [PictureModel], animatingDifferences: Bool) {
         snapshot.deleteAllItems()
-        snapshot.appendSections([Section.main])
-        snapshot.appendItems(pictures)
+        snapshot.appendSections(Section.allCases)
+        
+        snapshot.appendItems(pictures[0...9].map { $0 }, toSection: .carousel)
+        snapshot.appendItems(pictures[10...19].map { $0 }, toSection: .widget)
+        snapshot.appendItems(pictures[20...29].map { $0 }, toSection: .pinterest)
+
         dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
         isRefreshing = false
     }
